6afaae3f6d17b61947e5543fa914d76e551abd54
Yoon HeeSoo <improveme7@gmail.com>
2023-03-04 19:05:58 +0900

    나이 출력, 숫자 비교하기

---
+++ Algorithm/Programmers/Level0/나이 출력.cpp
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) {
+    int answer = 0;
+    answer = 2022 - (age - 1);
+    return answer;
+}
