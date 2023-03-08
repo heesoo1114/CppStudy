b9d11f58e7b5d87d2439e8849f4f2b39adf98c2b
Yoon HeeSoo <improveme7@gmail.com>
2023-03-09 00:10:24 +0900

    백준 20053 번외 풀이 추가

---
+++ Algorithm/Baekjoon/구현/소스.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	#pragma region 실행속도UP
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+#pragma endregion
+
+
+
+	return 0;
+}
