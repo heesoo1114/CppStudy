cadd960783e68750b92219d9684067c910e62031
Yoon HeeSoo <improveme7@gmail.com>
2023-03-14 23:34:49 +0900

    겜프_0314 숫자 기억 게임

--- GameProgramming/0314/0314_숫자 기억 게임.cpp
+++ GameProgramming/0314/0314_숫자 기억 게임.cpp
@@ -44,16 +44,29 @@ MODE selectMode(int input)
 	return mode;
 }
 
-void setGame(int &count)
+void setGame(int &count, int &delayTime)
 {
 	MODE mode;
 
 	cout << "(EASY : 1, NORMAL : 2, HARD : 3)";
-	cout << "수를 입력하여 모드를 설정하세요. : ";
+	cout << "수를 입력하여 모드를 설정하세요 : ";
 	
 	int input;
 	cin >> input;
 	mode = selectMode(input);
+
+	if (mode == 1)
+	{
+		delayTime = 1000;
+	}
+	else if (mode == 2)
+	{
+		delayTime = 700;
+	}
+	else
+	{
+		delayTime = 500;
+	}
 
 	count = mode * 5;
 }
@@ -64,15 +77,41 @@ int main()
 	cout << "       숫자 기억 게임		 " << endl;
 	cout << "----------------------------" << endl;
 
-	int count;
-	setGame(count);
+	int count, delayTime;
+	setGame(count, delayTime);
 
-	int nansu;
+	int nansu[15] = {};
+
+	// 숫자 보여주기
 	srand((unsigned int)time(NULL));
 	for (int i = 0; i < count; i++)
 	{
-		nansu = rand() % 100 + 1;
-		Sleep(1000);
-		cout << nansu << " ";
+		nansu[i] = rand() % 100 + 1;
+		cout << nansu[i] << " ";
+		
+		Sleep(delayTime);
 	}
+
+	system("cls");
+
+	// 입력 맞는지 확인
+	for (int i = 0; i < count; i++)
+	{
+		int num;
+		cin >> num;
+
+		if (num == nansu[i])
+		{
+			continue;
+		}
+		else
+		{
+			system("cls");
+			cout << "땡! 틀렸습니다!" << endl;
+			return 0;
+		}
+	}
+
+	system("cls");
+	cout << "축하합니다. 모두 맞추셨네요." << endl;
 }
