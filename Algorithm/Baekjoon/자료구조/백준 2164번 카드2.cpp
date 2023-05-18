6af2a7e1642b650b60862e58bd54deafd12b3a9a
Yoon HeeSoo <improveme7@gmail.com>
2023-05-18 14:54:21 +0900

    백준 2164번 카드2

---
+++ Algorithm/Baekjoon/자료구조/백준 2164번 카드2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	queue<int> q;
+
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		q.push(i);
+	}
+	
+	while (true)
+	{
+		if (q.size() == 1)
+		{
+			break;
+		}
+
+		q.pop();
+
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.front() << endl;
+}
